b12f8aa959b5d43ce898c46e74452daf181823e4
Joshua Chacko <Joshua.chacko.jc@gmail.com>
2026-05-19 20:22:18 -0500

    Added level of serverity and more user input for date and time

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,13 +1,45 @@
 filename = input("What is FileName: ")
+# Ask if you have a specific time and date
+if(input("Do you have a specific time and date to check")):
+    # too specify date filtering
+    date = input("What date in format: YR(XXXX)-MTH(XX)-DAY(XX)\n") 
+    # to specify time filtering
+    time = input("What time in format: XX:XX:XX\n")
 
-keywords = ['ERROR', 'FAILED', 'INVALID', 'LOCKED', 'UNAUTHROIZED', 'ANONYMOUS']
+# create a severity log style for filtering
+log_levels = {
+        'DEBUG' : ["DEBUG", "TRACE", "VERBOSE"],
+        'INFO' : ["INFO", "SUCCESS", "OK", 'STARTED', 'STOPPED', 'CONNECTED'],
+        'WARNING' : ['WARNING', 'WARN', 'DEPRECATED', 'SLOW', 'TIMEOUT', 'RETRY'],
+        'ERROR' : ['ERROR', 'FAILED', 'INVALID', 'DENIED', 'REFUSED', 'MISSING'],
+        'CRITICAL' : ['CRITICAL', 'FATAL', 'EMERGENCY', 'CORRUPT', 'CRASH', 'PANIC'],
+        'SECURITY_SPECIFIC' : ['UNAUTHORIZED', 'LOCKED', 'ANONYMOUS', 'BREACH', 'INTRUSION', 'BLOCKED', 'EXPLOIT']
+    }
+while True:
+    level = input("Enter log level to filter (DEBUG, INFO, WARNING, ERROR, CRITICAL, or SECURITY_SPECIFIC): ").upper()
+    if level in log_levels:
+        break
+    else:
+        print("Not a severity level")
+keywords = log_levels[level]
+warningLines = ""
 
 with open(filename, 'r') as file:
-    for line in file:
-        for keyword in keywords:
-            # improvement needed, what if a line contains multiple keywords
-            # it will print the same line multiple times
-            # we should keep track of all lines that are already listed as suspicous
-            # then create a verify for if that line is already checked
-            if keyword in line.upper():
-                print(line)
+    # if there is a date and time
+    if date and time:
+        for line in file:
+            # checking the lines that are only have the filter of date and time
+            if (date and time) in line:
+                for keyword in keywords:
+                    warningLines.add(line)
+    # if there is no provided date or time
+    else:
+        for line in file:
+            for keyword in keywords:
+                warningLines.add(line)
+        
+        
+
+# write all the given lines to the text file
+with open('error.txt', 'w'):
+    pass
